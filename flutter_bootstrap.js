25406a63bf6dc9f87d6bdb67e774a0490f6b6a89
Diego Selva <diegoselva92@gmail.com>
2024-11-04 12:02:22 -0300

    final_17

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1276846902"
+    serviceWorkerVersion: "1799973362"
   }
 });
