f87d5775b39e0c13357dcc02b1f9f81912fd6a65
Diego Selva <diegoselva92@gmail.com>
2024-11-04 11:38:36 -0300

    FINAL_MEPERDI

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1985000314"
+    serviceWorkerVersion: "2370809412"
   }
 });
